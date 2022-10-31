61691b72d5278d6314f14845ccb9faf9058ee422
AdilDev <adilexpo2@gmail.com>
2022-10-31 15:40:06 +0100

    JAVA basico Ejercio tema 1

--- src/com/ejercicioTema1/TiposDatos.java
+++ src/com/ejercicioTema1/TiposDatos.java
@@ -4,24 +4,35 @@ public class TiposDatos {
 
     public static void main(String[] args) {
 
-        // 1. numericos
+            // 1. numericos
 
-        // 1.1 enteris
-        byte variable1 = 5;
-        short variable2 = 10;
-        int variable3 = 30;
-        long variable4 = 100;
+            // 1.1 Numeros enteros
+            byte variable1 = 5;
+            short variable2 = 10;
+            int variable3 = 30;
+            long variable4 = 100;
 
-        // 1.2 decimales
-        float variable = 5.5f;
-        double variable6 = 10.5d;
+            System.out.println("Numeros enteros: " + variable1 + " , " + variable2 +  " , " + variable3 + " , " + variable4);
 
-        // 2 booleanos
-        boolean variable7 = false;
-        boolean variable8 = true;
+            // 1.2 Numeros decimales
+            float variable5 = 5.5f;
+            double variable6 = 10.5d;
 
-        // 3. texto
-        char variable9 = 'a';
-        String variable10 = "Lorem ipsum dolor sit amet....";
+            System.out.println("Numeros decimales: " + variable5 + " , " + variable6);
+
+
+
+            // 2 Numeros boleanos
+            boolean variable7 = false;
+            boolean variable8 = true;
+
+                System.out.println("Numeros boleanos: " + variable7 + " , " +  variable8);
+
+
+            // 3. texto
+            char variable9 = 'a';
+            String variable10 = "Lorem ipsum dolor sit amet....";
+
+            System.out.println("texto: " + variable9 + ", " + variable10);
     }
 }
