49e5248746c0a149257db1c4357eb0b70debda03
AdilDev <adilexpo2@gmail.com>
2022-10-31 13:18:34 +0100

    JAVA basico Ejercio tema 1

---
+++ src/com/ejercicioTema1/TiposDatos.java
@@ -0,0 +1,27 @@
+package com.ejercicioTema1;
+
+public class TiposDatos {
+
+    public static void main(String[] args) {
+
+        // 1. numericos
+
+        // 1.1 enteris
+        byte variable1 = 5;
+        short variable2 = 10;
+        int variable3 = 30;
+        long variable4 = 100;
+
+        // 1.2 decimales
+        float variable = 5.5f;
+        double variable6 = 10.5d;
+
+        // 2 booleanos
+        boolean variable7 = false;
+        boolean variable8 = true;
+
+        // 3. texto
+        char variable9 = 'a';
+        String variable10 = "Lorem ipsum dolor sit amet....";
+    }
+}
